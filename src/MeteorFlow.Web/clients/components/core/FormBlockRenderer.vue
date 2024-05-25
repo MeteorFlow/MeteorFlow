879d29ec3745cdab44704c4bf856e3add99e96aa
Thuy Nguyen <ducthuynguyen360@yahoo.com>
2024-05-25 16:35:07 +0700

    [FormBuilder] Implement a Drag'n'Drop Form builder

---
+++ src/MeteorFlow.Web/clients/components/core/FormBlockRenderer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { FormBlock } from "~/models/FormDefinition";
+import { InputType } from "~/models/FormDefinition";
+
+const props = defineProps<{
+  block: FormBlock;
+  mode: "editing" | "rendering";
+}>();
+</script>
+
+<template>
+  <div>
+    <label>{{ props.block.displayName }}</label>
+    <div v-if="props.block.element.schema.inputType === InputType.Text">
+      <CoreTextField :disabled="props.mode === 'editing' || props.block.extraConfig?.disabled" config class="disabled:cursor-pointer"></CoreTextField>
+    </div>
+    <div v-else-if="props.block.element.schema.inputType === InputType.Select">
+      <CoreSelect :items="['123', '456']"></CoreSelect>
+    </div>
+  </div>
+</template>
